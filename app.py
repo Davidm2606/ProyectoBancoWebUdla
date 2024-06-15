ed1dbf6938e67a90dd50182808e20dcd6c7176e1
Davidm2606 <davidteran2606@hotmail.com>
2024-06-15 01:18:41 -0500

    pp
    p

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import pymysql
 app = Flask(__name__)
 
 db = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='',
-    database='bancoweb',
+    host='os.getenv('DB_HOST')',
+    user='os.getenv('DB_USER')',
+    password='os.getenv('DB_PASSWORD')',
+    database='os.getenv('DB_NAME')',
+    port='os.getenv('DB_PORT')'
     cursorclass=pymysql.cursors.DictCursor
 )
 
